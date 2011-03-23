885e349a5df749439eb473d2ffb7419cec764448
d-rome <dromaryrxfloss@gmail.com>
2011-03-22 23:42:38 -0400

    Finished the password and authentication machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,45 @@ class User < ActiveRecord::Base
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..20 }
-              
+
+
+  before_save :encrypt_password
+
+# user = User.create!(:name => "Dan", :email => "dan@gmail.com", :password => "letmein", :password_confirmation => "letmein")
+
+  public
+
+    def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+    end
+
+
+  class << self
+  
+    def authenticate(email, submitted_password)
+      user = self.find_by_email(email)
+      return nil  if user.nil?
+      return user if user.has_password?(submitted_password)
+    end
+  end
+
+
+  private
+    
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,25 +60,27 @@ describe User do
     user_with_duplicate_email.should_not be_valid
   end
 
-
+# =========================================================
   describe "passwords" do
+# =========================================================
 
     before(:each) do
       @user = User.new(@attr)
     end
 
-    it "should have a password attribute" do
+    it "should exist as a User attribute" do
       @user.should respond_to(:password)
     end
 
-    it "should have a passowrd confirmation attribute" do
+    it "should also have a User password confirmation attribute" do
       @user.should respond_to(:password_confirmation)
     end  
   end
 
-
-  
+# =========================================================
   describe "password validation" do
+# =========================================================
+  
     it "should require a password" do
       User.new(@attr.merge(:password => "", :password_confirmation => "")).
         should_not be_valid
@@ -102,7 +104,9 @@ describe User do
     end
   end
 
+# =========================================================
   describe "password encryption" do
+# =========================================================
     
     before(:each) do
       @user = User.create!(@attr)
@@ -110,9 +114,50 @@ describe User do
     
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
-    end    
+    end
     
+    it "should set the encrypted password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
 
+
+    describe "has_password? method" do
+
+      it "should exist" do
+        @user.should respond_to(:has_password?)
+      end
+      
+      it "should return true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should respond false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end  
+
+
+    describe "authenitcation method" do
+
+      it "should respond with an object that exists" do
+        User.should respond_to(:authenticate)
+      end
+      
+      it "should return nil on email/password mismatch" do
+        User.authenticate(@attr[:email], "wrongpass").should be_nil
+      end
+      
+      it "should return nil for an email address with no stored user" do
+        User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+      end
+      
+      it "should return the user object on a successful email/password match" do
+        User.authenticate(@attr[:email], @attr[:password]).should == @user
+      end
+    end
   end
 end
 
