d00079b1b96a30b7a1f4c3120a1ab274955b0915
d-rome <dromaryrxfloss@gmail.com>
2011-03-24 02:48:32 -0400

    changed the about page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
   render_views
 
   before(:each) do
-  @base_title = "Ruby on Rails Tutorial Sample App"
+  @base_title = "MAP Drug Price Tracking Application"
   end
 
   describe "GET 'home'" do
